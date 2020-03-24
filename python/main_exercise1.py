286fbf9d51e2989f3e4715ce3e5af22ed6817bb1
Guillaume Viejo <guillaume.viejo@gmail.com>
2020-03-23 22:56:44 -0400

    New exercises

---
+++ python/main_exercise1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+'''
+	File name: main_exercise1.py
+	Author: Guillaume Viejo
+	Date created: 23/03/2020
+	Python Version: 3.6
+
+A set of basic exercises about numpy, pandas and matplotlib. 
+You should be able to write code without looking into main1_basics.py as much as possible
+'''
+
+###############################################################################
+# 1. Import numpy, matplotlib and pandas
+# Remember librairies have namespace
+###############################################################################
+
+
+###############################################################################
+# 2 define an array of 100000 random values between 0 and 1
+# Give it a memorable name
+###############################################################################
+
+
+###############################################################################
+# 3. Print mean, median, variance and standard deviation of your array
+###############################################################################
+
+
+###############################################################################
+# 4. Extract in an array the index of all values above 0.5 and in another array 
+# the values above 0.5
+# Hint : print(np.where.__doc__)
+# Memorable names forever
+###############################################################################
+
+
+###############################################################################
+# 5. Create a pandas series using the index and values above 0.5 from before
+# Hint : print(pd.Series.__doc__)
+###############################################################################
+
+
+###############################################################################
+# 7. Make a pandas series for you first array of values as well. 
+###############################################################################
+
+
+###############################################################################
+# 8. Plot the two series. The one with values above 0.5 should be plotted with 
+# dots only. The one with values between 0 and 1 with a straigth line
+###############################################################################
+
+
+###############################################################################
+# 9. At this point, you should learn to manipulate matplotlib figures to see 
+# each single point and make sure none is below 0.5. Try the zoom and pan in top left.
+###############################################################################
+
+
+###############################################################################
+# 10. Now two subplots with matplotblib (1 row, 2 columns). The first plot shows 
+# the same as before. The second plot shows the histogram of each pandas series.
+# Try to label the axis
+# Hint : use hist(my_serie, alpha = 0.5) for each series
+###############################################################################
---
+++ python/main_exercise2.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+'''
+	File name: main_exercise1.py
+	Author: Guillaume Viejo
+	Date created: 23/03/2020
+	Python Version: 3.6
+
+A bit more complex
+'''
+
+###############################################################################
+# 1. Import numpy, matplotlib and pandas
+###############################################################################
+
+
+###############################################################################
+# 2. Some parameters
+###############################################################################
+r = 0.3 # radius
+phi = 0.0 # angle
+omega = 3.4 # vitesse angulaire
+sigma  = -0.8 # parametre
+h = 0.001
+
+
+###############################################################################
+# 3 Define an integer n equal to 40000
+###############################################################################
+
+
+###############################################################################
+# 4 Define the variable called cosphi as r times cosinus of phi
+# and the variable sinphi as r times sinus of phi
+###############################################################################
+
+
+###############################################################################
+# 5 Use the variable n to define an array of zero of size (n, 5) called data
+###############################################################################
+
+
+
+###############################################################################
+# 6 Now make a loop iterating over n and define your iterator as i
+# Remember : everything inside the loop should be tabulated in regard for the for line.
+# To help you, the next steps are tabulated
+###############################################################################
+
+
+	
+	###############################################################################
+	# 7 Start with a condition : if i larger than 10000, assign 0.99 to sigma
+	###############################################################################
+	
+
+
+	###############################################################################
+	# 8 Second condition : if i larger than 30000, assign -0.99 to sigma
+	###############################################################################
+	
+
+
+	###############################################################################
+	# 9 Now to help you, the next line of code is already written 
+	###############################################################################
+	r = h*(np.tanh(sigma)*r - r**3.0) + r
+
+	###############################################################################
+	# 10 Assign to the variable phi the following value phi = h * omega + phi (yeah easy)
+	###############################################################################
+	
+
+	###############################################################################
+	# 11 Compute modulo 2*np.pi of phi and assign it to phi
+	# Hint : a%=(1) is a correct sentence in python
+	###############################################################################
+	
+
+	###############################################################################
+	# 12 Assign to the variables cosphi and sinphi the new value r times cosinus of phi
+	# and r times sinus of phi
+	###############################################################################
+
+
+	###############################################################################
+	# 13 Using the iterator i, assign at the ith posiiton in the first column the value r
+	# Remember in python, array indexing start at 0
+	###############################################################################
+
+	
+	###############################################################################
+	# 14 same for phi, cosphi, sinphi and sigma for columns 2, 3, 4 and 5
+	###############################################################################
+
+
+
+###############################################################################
+# 15 End of the loop. Now is time to plot the data. So make sure your code now is 
+# outside the loop. 
+# Make a two by two grid in matplotlib.
+# The top left plot should display the hyperbolic tangent of the sigma variable (in 
+# the last column of data)
+# The top right column should display the r variable (first column)
+# Bottom left should display cosphi and sinphi together. Try to put a legend to
+# differentiate the two
+# Bottom right should display cosphi versus sinphi
+###############################################################################
+
+
+
+###############################################################################
+# 16 If you code is correct, the plot should look like this :
+# https://www.dropbox.com/s/c58an08h235ztcj/figure_exercise2.png?dl=1
+# Congrats, you have simulated a Hopf bifurcation :
+# https://en.wikipedia.org/wiki/Hopf_bifurcation
+###############################################################################
+
