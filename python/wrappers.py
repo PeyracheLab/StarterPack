f2ef8c29a6e39a147373c001343c4e4f2aaae002
Guillaume Viejo <guillaume.viejo@gmail.com>
2020-05-21 16:02:43 -0400

    Fixed shank numbering

--- python/wrappers.py
+++ python/wrappers.py
@@ -51,15 +51,24 @@ def loadSpikeData(path, index=None, fs = 20000):
 			return spikes, shank
 		elif 'SpikeData.h5' in files:            
 			final_path = os.path.join(new_path, 'SpikeData.h5')            
-			final_path = os.path.join(new_path, 'SpikeData.h5')
-			store = pd.HDFStore(final_path, 'r')
-			toreturn = {}
-			for n in store['neurons_id'].values:
-				toreturn[n] = nts.Ts(store['neuron_'+str(n)].index.values, time_units = 's')			
-			
-			shank = store['shanks'].values
-			store.close()
-			return toreturn, shank
+			try:
+				spikes = pd.read_hdf(final_path, mode='r')
+				# Returning a dictionnary | can be changed to return a dataframe
+				toreturn = {}
+				for i,j in spikes:
+					toreturn[j] = nts.Ts(t=spikes[(i,j)].replace(0,np.nan).dropna().index.values, time_units = 's')
+				shank = spikes.columns.get_level_values(0).values[:,np.newaxis]
+				return toreturn, shank
+			except:
+				spikes = pd.HDFStore(final_path, 'r')
+				shanks = spikes['/shanks']
+				toreturn = {}
+				for j in shanks.index:
+					toreturn[j] = nts.Ts(spikes['/spikes/s'+str(j)])
+				shank = shanks.values
+				spikes.close()
+				del spikes
+				return toreturn, shank
 			
 		else:            
 			print("Couldn't find any SpikeData file in "+new_path)
@@ -68,46 +77,75 @@ def loadSpikeData(path, index=None, fs = 20000):
 	# Creating /Analysis/ Folder here if not already present
 	if not os.path.exists(new_path): os.makedirs(new_path)
 	files = os.listdir(path)
-	clu_files     = np.sort([f for f in files if '.clu.' in f and f[0] != '.'])
-	res_files     = np.sort([f for f in files if '.res.' in f and f[0] != '.'])
+	clu_files     = np.sort([f for f in files if 'clu' in f and f[0] != '.'])
+	res_files     = np.sort([f for f in files if 'res' in f and f[0] != '.'])
 	clu1         = np.sort([int(f.split(".")[-1]) for f in clu_files])
 	clu2         = np.sort([int(f.split(".")[-1]) for f in res_files])
 	if len(clu_files) != len(res_files) or not (clu1 == clu2).any():
 		print("Not the same number of clu and res files in "+path+"; Exiting ...")
 		sys.exit()
-
-	count = 0    
-	neurons = {}
-	shank = []
-
-	for i in range(len(clu_files)):
-		clu = np.genfromtxt(os.path.join(path,clu_files[i]),dtype=np.int32)[1:]
+	count = 0
+	spikes = []
+	basename = clu_files[0].split(".")[0]
+	for i, s in zip(range(len(clu_files)),clu1):
+		clu = np.genfromtxt(os.path.join(path,basename+'.clu.'+str(s)),dtype=np.int32)[1:]
 		if np.max(clu)>1:
-			res = np.genfromtxt(os.path.join(path,res_files[i]))
+			# print(i,s)
+			res = np.genfromtxt(os.path.join(path,basename+'.res.'+str(s)))
 			tmp = np.unique(clu).astype(int)
 			idx_clu = tmp[tmp>1]
-			idx_neu = np.arange(count, count+len(idx_clu))
-			for j, n in zip(idx_clu, idx_neu):
-				neurons[n] = pd.Series(index = np.unique(res[clu==j])/fs, data = np.uint8(n), dtype = np.uint8)
-				shank.append(int(clu_files[i].split(".")[-1]))
-			count += len(idx_clu)
-	   
-	# Saving SpikeData.h5
-	final_path = os.path.join(new_path, 'SpikeData.h5')
-	store = pd.HDFStore(final_path, 'w')
-	for n in neurons:
-		store.append('neuron_'+str(n), neurons[n])
-	store.append('neurons_id', pd.Series(np.array(list(neurons.keys()))))
-	store.append('shanks', pd.Series(shank))
-	store.close()
+			idx_col = np.arange(count, count+len(idx_clu))	        
+			tmp = pd.DataFrame(index = np.unique(res)/fs,
+								columns = pd.MultiIndex.from_product([[s],idx_col]),
+								data = 0, 
+								dtype = np.uint16)
+			for j, k in zip(idx_clu, idx_col):
+				tmp.loc[res[clu==j]/fs,(s,k)] = np.uint16(k+1)
+			spikes.append(tmp)
+			count+=len(idx_clu)
+
+			# tmp2 = pd.DataFrame(index=res[clu==j]/fs, data = k+1, ))
+			# spikes = pd.concat([spikes, tmp2], axis = 1)
 
 
 	# Returning a dictionnary
-	toreturn = {}
-	for i in neurons:
-		toreturn[i] = nts.Ts(t = neurons[i].index.values, time_units = 's')
-	
+	toreturn =  {}
+	shank = []
+	for s in spikes:
+		shank.append(s.columns.get_level_values(0).values)
+		sh = np.unique(shank[-1])[0]
+		for i,j in s:
+			toreturn[j] = nts.Ts(t=s[(i,j)].replace(0,np.nan).dropna().index.values, time_units = 's')
+
+	del spikes
+	shank = np.hstack(shank)
+
+	final_path = os.path.join(new_path, 'SpikeData.h5')
+	store = pd.HDFStore(final_path)
+	for s in toreturn.keys():
+		store.put('spikes/s'+str(s), toreturn[s].as_series())
+	store.put('shanks', pd.Series(index = list(toreturn.keys()), data = shank))
+	store.close()
+
+	# OLD WAY
+	# spikes = pd.concat(spikes, axis = 1)
+	# spikes = spikes.fillna(0)
+	# spikes = spikes.astype(np.uint16)
+
+	# Saving SpikeData.h5
+	# final_path = os.path.join(new_path, 'SpikeData.h5')
+	# spikes.columns.set_names(['shank', 'neuron'], inplace=True)    
+	# spikes.to_hdf(final_path, key='spikes', mode='w')
+
+	# Returning a dictionnary
+	# toreturn = {}
+	# for i,j in spikes:
+	# 	toreturn[j] = nts.Ts(t=spikes[(i,j)].replace(0,np.nan).dropna().index.values, time_units = 's')
+
+	# shank = spikes.columns.get_level_values(0).values[:,np.newaxis].flatten()
+
 	return toreturn, shank
+
 
 def loadXML(path):
 	"""
